e7e299996f16f9f43d6143ae5f6773b79e75378c
gauthier.cedrick <cedrick_g@proton.me>
2025-08-26 19:22:25 -0400

    lint spinach

--- app/spinach.py
+++ app/spinach.py
@@ -1,16 +1,22 @@
+"""The spinach language"""
+
 from app.frontend.spinach_front import SpinachFront
 from app.backend.spinach_back import SpinachBack
 from app.ast_builder import AstBuilder
 
 
 class Spinach:
+    """The spinach language"""
+
     @staticmethod
     def creat_circuit(code: str):
+        """generate a tket circuit from spinach code"""
         built = AstBuilder().transform(SpinachFront.get_tree(code))
         return SpinachBack.compile_to_circuit(built)
 
     @staticmethod
     def compile(code: str, language: str) -> str:
+        """translate spinach code to other languages"""
         dispatch = {
             "qasm": SpinachBack.compile_to_openqasm,
             "json": SpinachBack.compile_to_json,
