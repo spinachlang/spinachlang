1081c714aaa208dcbae718aef4f244e002bef254
gauthier.cedrick <cedrick_g@proton.me>
2025-08-26 21:08:34 -0400

    lint spinach front

--- app/frontend/spinach_front.py
+++ app/frontend/spinach_front.py
@@ -1,11 +1,16 @@
-from lark import Lark
+"""frontend of spinach"""
+
 from pathlib import Path
+from lark import Lark
 
 
-class SpinachFront:
+class SpinachFront:  # pylint: disable=too-few-public-methods
+    """frontend of spinach"""
+
     @staticmethod
     def get_tree(code: str):
-        with open(Path("app") / "frontend" / "grammar.lark") as f:
+        """generate tree out of spinach code"""
+        with open(Path("app") / "frontend" / "grammar.lark", encoding="utf-8") as f:
             grammar = f.read()
         parser = Lark(grammar, start="start", parser="lalr")
         return parser.parse(code)
