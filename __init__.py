6a21f23654cb5cd695e264232ee7949350603cd0
Cédrick Gauthier <cedrick_g@proton.me>
2025-09-24 00:47:36 -0400

    Make it releasble (#17)
    * add init and pyproject files
    
    * remove unwanted prints
    
    * add release action
    
    * lint
    
    * lint
    
    * lint
    
    * fix tests
    
    * lint
    
    * fix tests
    
    * fix tests
    
    * make package numbers automatic

---
+++ __init__.py
@@ -0,0 +1,12 @@
+"""
+SpinachLang package.
+
+This package provides the Spinach language implementation.
+The main entry point is `SpinachLang`, which can be imported as:
+
+    from app.spinach import SpinachLang
+"""
+
+from .app.spinach import Spinach as SpinachLang
+
+__all__ = ["SpinachLang"]
--- __main__.py
+++ __main__.py
@@ -1,6 +1,6 @@
 """spinach CLI entry point"""
 
-from app.main import main
+from .app.main import main
 
 if __name__ == "__main__":
     main()
