18c528544617051161313d1b0caf48f102295d83
gauthier.cedrick <cedrick_g@proton.me>
2025-10-09 13:29:03 -0400

    modify __main__.py

--- spinachlang/__main__.py
+++ spinachlang/__main__.py
@@ -1,6 +1,7 @@
-"""spinach CLI entry point"""
+"""Spinach API entry point"""
 
-from .main import main
+import uvicorn
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run("spinachlang.main:app", host="0.0.0.0", port=8000, reload=True)
+
